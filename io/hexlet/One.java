8f31ec29f171c9578f848cef9b4cc6dbc6e31a70
Sergey Medvedev <s.medvedev86@mail.ru>
2022-04-16 15:16:52 +0300

    update One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,8 +1,13 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
+	
 	public static void main(String[] args){
-		Two.greeting();
+		//Two.greeting();
+		String hexlet = "Hexlet";
+		System.out.println(StringUtils.reverse(hexlet));
 	}
 }
 
